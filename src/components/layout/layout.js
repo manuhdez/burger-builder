321a0631be9c6cd3f382c3b6265c2f8e40643767
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-04-18 13:19:42 +0100

    Added CSS file to layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/aux';
 
+import classes from './layout.css';
+
 const layout = (props) => (
   <Aux>
     <div>toolbar, sidedrawer, backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
