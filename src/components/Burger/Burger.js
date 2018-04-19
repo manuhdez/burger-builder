bf0ad10ca247e9597816b11f81ad7cc1af88c384
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-04-19 10:54:17 +0100

    Added calc of the actual ingredients to display

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import classes from './Burger.css';
 
 const burger = (props) => {
 
-  const transformedIngredients = Object.keys(props.ingredients).map(
+  let transformedIngredients = Object.keys(props.ingredients).map(
     igKey => [...Array(props.ingredients[igKey])].map((_, i) => {
       return <BurgerIngredient key={igKey + i} type={igKey} />;
     })
-  );
+  ).reduce((arr, el) => arr.concat(el), []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
 
   return (
     <div className={classes.Burger}>
