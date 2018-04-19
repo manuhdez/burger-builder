63c0450abd6ac015e328c301a36b22d789cfa30b
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-04-19 12:22:30 +0100

    Added controls and functionality to add and remove ingredients and update the price

--- src/containers/burgerBuilder/burgerBuilder.js
+++ src/containers/burgerBuilder/burgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/buildControls/buildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.4,
+  cheese: 0.3,
+  bacon: 0.7,
+  meat: 1.3
+}
+
 class BurgerBuilder extends Component {
 
   state = {
@@ -11,14 +18,52 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
   }
 
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const newCount = oldCount + 1;
+    const updateIngredients = { ...this.state.ingredients };
+    updateIngredients[type] = newCount;
+
+    const addPrice = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + addPrice;
+
+    this.setState({ totalPrice: newPrice, ingredients:  updateIngredients});
+  };
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const newCount = oldCount - 1;
+    const updateIngredients = { ...this.state.ingredients };
+    updateIngredients[type] = newCount;
+
+    const removePrice = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - removePrice;
+
+    this.setState({ totalPrice: newPrice, ingredients:  updateIngredients});
+  };
+
   render() {
+    const disabledInfo = { ...this.state.ingredients };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+        ingredientAdded={this.addIngredientHandler}
+        ingredientRemoved={this.removeIngredientHandler}
+        disabled={disabledInfo}  />
       </Aux>
     );
   }
