72bc13c44056374202bbbef82e1339d8bac511de
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-04-20 14:03:35 +0100

    Added the logo component and displayed it in the toolbar

--- src/components/navigation/toolbar/toolbar.js
+++ src/components/navigation/toolbar/toolbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import classes from './toolbar.css';
+import Logo from '../../logo/logo';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-
+  <div>Menu</div>
+  <Logo />
   </header>
 );
 
