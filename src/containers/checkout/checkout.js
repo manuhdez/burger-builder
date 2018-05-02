d4b44fe906186df22601e943ca71fe0891a1d283
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-05-02 23:10:19 +0100

    Added checkoutSummary basic functionality

--- src/containers/checkout/checkout.js
+++ src/containers/checkout/checkout.js
@@ -11,10 +11,21 @@ class Checkout extends Component {
     }
   }
 
+  checkoutCancelHandler = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinueHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancel={this.checkoutCancelHandler}
+          checkoutContinue={this.checkoutContinueHandler} />
       </div>
     )
   }
