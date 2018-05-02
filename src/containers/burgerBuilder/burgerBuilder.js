4c3dab05509d2de18ebb588f0c429189e7a880d9
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-05-02 23:02:57 +0100

    Added navigation to the checkout page

--- src/containers/burgerBuilder/burgerBuilder.js
+++ src/containers/burgerBuilder/burgerBuilder.js
@@ -81,26 +81,27 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continued!');
-    this.setState({loading: true});
+    // this.setState({loading: true});
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Manu Hdez',
-        address: {
-          street: 'Paseo del Prado, 23',
-          zipCode: '35210',
-          country: 'Spain'
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest'
-    };
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Manu Hdez',
+    //     address: {
+    //       street: 'Paseo del Prado, 23',
+    //       zipCode: '35210',
+    //       country: 'Spain'
+    //     },
+    //     email: 'test@test.com',
+    //   },
+    //   deliveryMethod: 'fastest'
+    // };
 
-    axios.post('/orders.json', order)
-      .then( response => this.setState({loading: false, purchasing: false}))
-      .catch( error => this.setState({loading: false, purchasing: false}));
+    // axios.post('/orders.json', order)
+    //   .then( response => this.setState({loading: false, purchasing: false}))
+    //   .catch( error => this.setState({loading: false, purchasing: false}));
+    this.props.history.push('/checkout');
   };
 
   render() {
